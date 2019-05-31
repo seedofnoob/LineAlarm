7f19a3cd5e0c273db8d0b7d0347cf7adefe64a76
seedofnoob <50301559+seedofnoob@users.noreply.github.com>
2019-05-31 22:31:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('8+wpbLOUtVsyaCZEKB7TUSJLib22ZmYYJ2XcsWD47ZxRr67Dxe/XLKrKDrN0JMd5H3YVveNbL2v1rcwwqIXrkma8cbTJUxOumZe/aTPSYPpf6l0YgqI3/mgnb0fR2gWfkrDzBgXHpbp0yW8R/RvlqgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('4ba0109043212761ae0eb99abbbf28d8')
 
 app = Flask(__name__)
 
 # username of anto.io account
-user = 'YOUR_USERNAME'
+#user = 'YOUR_USERNAME'
 # key of permission, generated on control panel anto.io
-key = 'YOUR_KEY'
+#key = 'YOUR_KEY'
 # your default thing.
-thing = 'YOUR_THING'
+#thing = 'YOUR_THING'
 
-anto = antolib.Anto(user, key, thing)
+#anto = antolib.Anto(user, key, thing)
 
 
 @app.route("/callback", methods=['POST'])
@@ -46,10 +46,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text="Turn Off channel1"))
+    if(message == 'on' or message == 'เปิด'):
+        line_bot_api.reply_message(
+        event.reply_token,TextSendMessage(text="เปิดสัญญาณกันขโมย"))
+    elif(message == 'off' or message == 'ปิด'):
+        line_bot_api.reply_message(
+        event.reply_token,TextSendMessage(text="ปิดสัญญาณกันขโมย"))
+    else:
+        line_bot_api.reply_message(
+        event.reply_token,TextSendMessage(text="งง"))
 
 if __name__ == "__main__":
-    anto.mqtt.connect()
+#    anto.mqtt.connect()
     app.run(debug=True)
